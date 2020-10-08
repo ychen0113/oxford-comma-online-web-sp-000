ba070aac2f17d1404f546cdf62828239a9d912db
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-08 17:31:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,10 @@ def oxford_comma(array)
  if array.length == 1 
    array.join
  elsif array.length == 1 
- 
+  y = array[-1]
+  array[-1] = "and #{y}"
+  array.join
+  else 
   x = array[-1]
   array[-1] = "and #{x}"
   array.join(", ")
