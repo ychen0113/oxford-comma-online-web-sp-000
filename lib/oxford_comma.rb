98ceee6bdfe4facf2772d8fb632a5cb00b6086b8
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-08 17:31:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+ if array.length == 1 
+   array.join
+ elsif array.length == 2
+  y = array[-1]
+  array[-1] = " and #{y}"
+  array.join
+  else 
+  x = array[-1]
+  array[-1] = "and #{x}"
+  array.join(", ")
+end 
 end
